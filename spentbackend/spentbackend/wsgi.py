fb66e6d1c121326f5290336f26146e4baf414cf7
Amrit Sundarka <amritsundarka2004@gmail.com>
2024-07-31 18:07:46 +0530

    changed

--- spentbackend/spentbackend/wsgi.py
+++ spentbackend/spentbackend/wsgi.py
@@ -1,13 +1,9 @@
-"""
-WSGI config for spentbackend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the project root to the Python path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
 
 from django.core.wsgi import get_wsgi_application
 
