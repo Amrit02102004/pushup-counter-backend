de7fe58b5c1e3caadcb580f419577947d40c6dcc
Amrit Sundarka <amritsundarka2004@gmail.com>
2024-08-03 20:10:48 +0530

    deleted pushup[s

--- spentbackend/Pushups/urls.py
+++
@@ -1,8 +0,0 @@
-# Pushups/urls.py
-
-from django.urls import path
-from .views import process_image
-
-urlpatterns = [
-    path('process/', process_image, name='process_image'),
-]
--- spentbackend/spentbackend/settings.py
+++ spentbackend/spentbackend/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'Profile',
     'Login',
-    'Pushups',
 ]
 
 MIDDLEWARE = [
